fb3990f32a3b87d1f9490e906b738608b29ba091
muller.mendes <muller.mendes@cnj.jus.br>
2017-10-06 10:06:44 -0300

    primeiro commit de inicio de projeto.

---
+++ src/br/com/framework/implementacao/crud/VariavelConexaoUtil.java
@@ -0,0 +1,13 @@
+package br.com.framework.implementacao.crud;
+
+
+/**
+ * Nome do caminho do JNDI datasource TomCat
+ * @author muller.mendes
+ *
+ */
+public class VariavelConexaoUtil {
+	
+	public static String JAVA_COMP_ENV_JDBC_DATA_SOURCE = "java:/comp/env/jdbc/datasource";
+
+}
